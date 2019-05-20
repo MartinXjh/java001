8f1a8a414368253d72bdf6b6237eb2fc07b3bf27
MartinXjh <50827034+MartinXjh@users.noreply.github.com>
2019-05-20 19:52:27 +0800

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,7 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//EEEEEEE
+
 @SpringBootApplication
 public class RestfulApplication {
 
