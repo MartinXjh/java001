8c80a6d6b0e47675e8930c56f95e6050c945a806
offcn-java <xujh@hvgroup.com.cm>
2019-07-28 16:42:46 +0800

    TEST

--- 1.java
+++ 1.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfulApplication {
 
     public static void main(String[] args) {
+        System.out.println("11");
         SpringApplication.run(RestfulApplication.class, args);
     }
 
